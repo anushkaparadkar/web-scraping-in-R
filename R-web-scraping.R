b5ec8af18f7f17c6fd26cd51945e7b1c4deb7cf1
Anushka Paradkar <anupara15@gmail.com>
2020-04-02 12:28:28 +0530

    updated readme

--- R-web-scraping.R
+++ R-web-scraping.R
@@ -16,28 +16,3 @@ top_artists <- tibble('Artist' = gsub("\n", "", artist),'Rank' = rank) %>%
   filter(rank <= 10)
 top_artists
 
-
-#Format the link to navigate to the artists genius webpage
-genius_urls <- paste0("https://genius.com/artists/",top_artists$Artist)
-
-# Outer loop to get the song links for each artist 
-for (i in 1:10) {
-  genius_page <- read_html(genius_urls[i])
-  song_links <- html_nodes(genius_page, ".mini_card_grid-song a") %>%html_attr("href") 
-    #Inner loop to get the Song Name   
-  for (j in 1:10) {
-    # Get song name
-    song_name <- read_html(song_links[j]) %>%html_nodes("h1.header_with_cover_art-primary_info-title") %>%html_text()
-    
-    # Save the details to a tibble
-    artist_lyrics <- rbind(artist_lyrics, tibble(Rank = top_artists$Rank[i],
-                                                 Artist = top_artists$Artist[i],
-                                                 Song = song_name ))
-    
-    # Insert a time lag - to prevent me from getting booted from the site :)
-    Sys.sleep(10)
-  }
-} 
-
-#Inspect the results
-artist_lyrics                      
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # web-scraping-in-R
-A simple implementation of web scraping in R
+
+A simple implementation of web scraping in R.I have scraped the Billboard website to get top 10 artists. This implementation can be expanded to collection of any data and hence you can make your own data sets.
+
+I have used R programming language's 'rvest' package for web-scraping
+
+##Prerequisite
+
+You will need to install and load the following packages.  
+install.packages('rvest')
+install.packages('tidyverse')
+
+library(tidyverse)
+library(rvest)
+
+##Some Theory
+I have used the following functions in my code -
+
+1.read_html() - Generation of a HTML document from the URL
+2.html_nodes() -The fucntion helps you extract the data you want based on its CSS selector.
